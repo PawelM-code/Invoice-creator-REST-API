919b12480efee200b733f9f11983553e35c9b7bc
PawelM-code <maciejewski.pawel@o2.pl>
2020-02-17 11:21:06 +0100

    first commit

---
+++ src/test/java/com/app/invoicecreator/InvoiceCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.invoicecreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
