76e3ab4c0f7920d28ad8b2d351559fa7513d494f
PawelM-code <maciejewski.pawel@o2.pl>
2020-02-29 11:11:00 +0100

    add ConstraintViolationException

--- src/main/java/com/app/invoicecreator/service/ProductService.java
+++ src/main/java/com/app/invoicecreator/service/ProductService.java
@@ -3,14 +3,18 @@ package com.app.invoicecreator.service;
 import com.app.invoicecreator.domain.product.Product;
 import com.app.invoicecreator.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+    private final static Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
     private final ProductRepository productRepository;
 
     public Product saveProduct(final Product product) {
@@ -18,7 +22,11 @@ public class ProductService {
     }
 
     public void deleteProduct(Long id) {
-        productRepository.deleteById(id);
+        try {
+            productRepository.deleteById(id);
+        } catch (ConstraintViolationException e){
+            LOGGER.error(e.getMessage(), e);
+        }
     }
 
     public Optional<Product> getProduct(Long id) {
